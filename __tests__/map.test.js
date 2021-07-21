53e201ff87f686b6c4277f19f641466b78a0b6c7
Kubi Kubisiak <mckubisiak@gmail.com>
2021-07-21 09:22:34 -0700

    set up intial test for map current failing

---
+++ __tests__/map.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable indent */
+const { myMap } = require('./map');
+
+
+describe('myMap', () => {
+
+    it('takes an array and a function(callback) to create a new array modified by the callback', () => {
+
+        const callBackFunction = (identifer) => identifer + '! What did you say to me?';
+        const actual = myMap(['PUNK', 'Mom', 'officer'], callBackFunction);
+        const expected =  ['PUNK! What did you say to me?', 'Mom! What did you say to me?', 'officer! What did you say to me?'];
+
+        expect(actual).toEqual(expected);
+    });
+});
+
+
+
+
